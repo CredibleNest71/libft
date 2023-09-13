5039ec074394f0bf65a76ac0fe88dbbbd0516f4b
CredibleNest71 <144441212+CredibleNest71@users.noreply.github.com>
2023-09-13 14:12:45 +0000

    almost perfect

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 11:14:51 by codespace         #+#    #+#             */
-/*   Updated: 2023/09/13 13:03:40 by codespace        ###   ########.fr       */
+/*   Updated: 2023/09/13 14:11:17 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,27 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*newlst;
+	t_list	*temp;
 
 	newlst = NULL;
+	if (!(lst && f && del))
+		return (0);
 	while (lst)
 	{
-		ft_lstadd_back(&newlst, f(lst->content));
+		temp = ft_lstnew(f(lst->content));
+		if (!temp)
+		{
+			while (temp)
+			{
+				temp = newlst->next;
+				del(newlst);
+				free(newlst);
+				newlst = temp;
+			}
+			return (0);
+		}
+		ft_lstadd_back(&newlst, temp);
 		lst = lst->next;
 	}
-	ft_lstclear(&lst, del);
 	return (newlst);
 }
