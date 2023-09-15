328d8b66f21fecbc2acc060c70abd4634d316cb5
CredibleNest71 <144441212+CredibleNest71@users.noreply.github.com>
2023-09-15 10:55:21 +0000

    geht schon

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 11:14:51 by codespace         #+#    #+#             */
-/*   Updated: 2023/09/13 14:11:17 by codespace        ###   ########.fr       */
+/*   Updated: 2023/09/15 10:52:39 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,15 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*newlst;
 	t_list	*temp;
 
-	newlst = NULL;
-	if (!(lst && f && del))
+	newlst = 0;
+	if (!(f && del))
 		return (0);
 	while (lst)
 	{
 		temp = ft_lstnew(f(lst->content));
 		if (!temp)
 		{
-			while (temp)
-			{
-				temp = newlst->next;
-				del(newlst);
-				free(newlst);
-				newlst = temp;
-			}
+			ft_lstclear(&newlst, del);
 			return (0);
 		}
 		ft_lstadd_back(&newlst, temp);
